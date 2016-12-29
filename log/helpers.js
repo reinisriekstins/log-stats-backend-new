506faed1333c3fb21fe360138bbbce73d39cfc47
Reinis Riekstins <rr14022@lu.lv>
2016-12-29 18:49:58 +0200

    separated router from middleware

--- log/helpers.js
+++ log/helpers.js
@@ -1,248 +1,24 @@
 const
   // { join } = require('path'),
   // axios = require('axios')
-  { Observable } = require('rxjs/Rx'),
-  { isError,
-    isObject } = require('lodash'),
-  { load } = require('cheerio')
+  { Observable } = require('rxjs/Rx')
+  ,isError = require('lodash/fp/isError')
+  ,isObject = require('lodash/fp/isObject')
+  ,isString = require('lodash/fp/isString')
+  ,{ load } = require('cheerio')
 
-/*
-function (logId) {
-
-  ///// needs debugging
-  const
-    htmlUrl = join('http://logs.tf/', logId),
-    jsonUrl = join('http://logs.tf/json/', logId)
-
-  const json$ = Observable
-    .fromPromise(axios(jsonUrl))
-    //.map(x => x.data)
-    .map(validateLogJson)
-    .retryWhen(tenTimesWithDelay$)
-
-  const html$ = Observable
-    .fromPromise(axios(htmlUrl))
-    //.do(console.log, console.warn, console.info)
-    //.map(x => x.data)
-    .map(validateAndMineLogHtml)
-    .retryWhen(tenTimesWithDelay$)
-
-  // combined lastest from html$ and json$
-  const combined$ = Observable
-    .combineLatest(json$, html$, () => {
-      if (isError(json$))
-        return new Error('JSON is error:' + json$)
-      else if (isError(html$))
-        return new Error('HTML is error:' + html$)
-      /// maybe embed the html derived data
-      /// in the log json?
-      return { json: json$, html: html$ }
-    })
-
-  /// does this even do anything?
-  function validateLogJson(x) {
-    if (isError(x)) return x
-    try {
-      return x
-    } catch(e) {
-      return new Error(`{Error: ${e}, Json: ${x}}`)
-    }
-  }
-
-  function validateAndMineLogHtml(x) {
-    const
-      $ = load(x),
-      h3 = $('.log-section > h3').html()
-
-    if ( h3 === 'Something went wrong' )
-      return new Error('logs.tf "Something went wrong" page.')
-
-    // filter out only the necessary data
-    const
-      map  = $('#log-map').html() || '',
-      time = $('.datefield').attr('data-timestamp')
-
-    if (time === undefined)
-      return new Error('Couldn\'t find data-timestamp.')
-
-    return { logId, map, time }
-  }
-
-  function tenTimesWithDelay$(error$) {
-    const delay = 10 // ms
-    return error$
-      .scan((count, err) => {
-        ++count
-        return (count >= 10) ? err : count
-      }, 0)
-      .takeWhile(count => count < 10)
-      // .do(count =>
-      //    console.log(`Retry #${count} in ${delay}ms...`))
-      .delay(delay)
-  }
-
+module.exports = (function () {
   return {
-    json$,
-    html$,
-    combined$,
-    validateLogJson,
-    validateAndMineLogHtml,
-    tenTimesWithDelay$
-  }
-}
-*/
-/*
-module.exports = function (logId = null) {
-  //// does this even do anything useful?
-  function validateLogJson(x) {
-    if (isError(x)) return x
-    try {
-      return x
-    } catch(e) {
-      return new Error(`{Error: ${e}, Json: ${x}}`)
-    }
-  }
-
-  function validateAndMineLogHtml(htmlStr) {
-    const
-      $ = load(htmlStr),
-      h3 = $('.log-section > h3').html()
-
-    if ( h3 === 'Something went wrong' )
-      return new Error('logs.tf "Something went wrong" page.')
-
-    // filter out only the necessary data
-    const
-      map  = $('#log-map').html() || '',
-      time = $('.datefield').attr('data-timestamp')
-
-    if (time === undefined)
-      return new Error('Couldn\'t find data-timestamp.')
-
-    return { logId, map, time }
-  }
-
-  function TenTimesWithDelay$(error$) {
-    const delay = 10 // ms
-    return error$
-      .scan((count, err) => {
-        ++count
-        return (count >= 10) ? err : count
-      }, 0)
-      .takeWhile(count => count < 10)
-      // .do(count =>
-      //    console.log(`Retry #${count} in ${delay}ms...`))
-      .delay(delay)
-  }
-
-  //// combine createJson$ and createHtml$
-  //// into a single factory functino, in which
-  //// you pass in the promise and validation function?
-  function PromisedValidated$(promise, validationFn) {
-    return Observable
-      .fromPromise(promise)
-      //.map(x => x.data)
-      .map(validationFn)
-      .retryWhen(tenTimesWithDelay$)
-  }
-
-  let json$, html$, combined$
-
-  function Combined$(first$ = json$, second$ = html$) {
-    return Observable
-      .combineLatest(first$, second$, () => {
-        if (isError(first$))
-          return new Error('JSON is error:' + first$)
-        else if (isError(second$))
-          return new Error('HTML is error:' + second$)
-        /// maybe embed the html derived data
-        /// in the log json?
-        return { json: first$, html: second$ }
-      })
-  }
-
-  function Subscribe$(obs$, ...subscribeArgs) {
-    if ( isObject(subscribeArgs[0]) )
-      return obs$.subscribe(subscribeArgs[0])
-    else
-      return obs$.subscribe(...subscribeArgs)
-  }
-
-
-  const API = {
-    logId,
-    validateLogJson,
-    validateAndMineLogHtml,
-    tenTimesWithDelay,
-    PromisedValidated$,
-    Combined$,
-    Subscribe$,
-    json(promise, ...subscribeArgs) {
-      API.json$(promise, ...subscribeArgs)
-      return API
-    },
-    html(promise, ...subscribeArgs) {
-      API.html$(promise, ...subscribeArgs)
-      return API
-    },
-    json$(promise, validationFn, ...subscribeArgs) {
-      json$ =
-        PromisedValidated$(promise, validateLogJson)
-
-      if (subscribeArgs.length)
-        return Subscribe$(json$, ...subscribeArgs)
-      return json$
-    },
-    html$(promise, validationFn, ...subscribeArgs) {
-      html$ =
-        PromisedValidated$(promise, validateAndMineLogHtml)
-
-      if (subscribeArgs.length)
-        return Subscribe$(html$, ...subscribeArgs)
-      return html$
-    },
-    combined$(first$, second$, ...subscribeArgs) {
-      combined$ =
-        Combined$(first$ || json$, second$ || html$)
-
-      if (subscribeArgs.length)
-        return Subscribe$(combined$, ...subscribeArgs)
-      return combined$
-    }
-  }
-  return API
-}
-*/
-
-module.exports = function () {
-  // private:
-  function TenTimesWithDelay$(error$) {
-    const delay = 10 // ms
-    return error$
-      .scan((count, err) => {
-        ++count
-        return (count >= 10) ? err : count
-      }, 0)
-      .takeWhile(count => count < 10)
-      // .do(count =>
-      //   console.log(`Retry #${count} in ${delay}ms...`))
-      .delay(delay)
-  }
-
-  // public:
-  return {
-    // does this one even do something useful?
     validateLogJson(x) {
-      if (isError(x)) return x
-      try {
+      if (isError(x))
         return x
-      } catch(e) {
-        return new Error(`{Error: ${e}, Json: ${x}}`)
-      }
+      else if (isString(x.data))
+        return new Error(`Response is a string, not an object: ${x.data}`)
+      return x
     },
     validateAndMineLogHtml(htmlStr) {
       const
-        $ = load(htmlStr),
+        $ = load(htmlStr.data),
         h3 = $('.log-section > h3').html()
 
       if ( h3 === 'Something went wrong' )
@@ -257,30 +33,42 @@ module.exports = function () {
         return new Error('Couldn\'t find data-timestamp.')
 
       return { map, time }
-    },
-    PromisedValidated$(promise, validationFn = x => x) {
+  },
+    TakeNTimesWithDelay$(config) {
+      const {
+        promise,
+        times = 10,
+        delay = 10,
+        validationFn = x => x
+      } = config
+
       return Observable
-        .fromPromise(promise)
-        //.map(x => x.data)
+        .interval(delay)
+        .startWith(0)
+        .take(times)
+        // .do(x => console.log('emitting', x))
+        .flatMap(promise)
         .map(validationFn)
-        .retryWhen(tenTimesWithDelay$)
+        .first(x => !isError(x))
     },
-    Combined$(first$, second$) {
-    return Observable
-      .combineLatest(first$, second$, () => {
-        if (isError(first$))
-          return new Error('JSON is error:' + first$)
-        else if (isError(second$))
-          return new Error('HTML is error:' + second$)
-        /// maybe embed the html derived data
-        /// in the log json? or return array?
-        return { json: first$, html: second$ }
-      })
+    Combined$(one$, two$) {
+      return Observable
+        .combineLatest(one$, two$, (one, two) => {
+          if (isError(one)) {
+            return one
+          }
+          else if (isError(two)) {
+            return two
+          }
+          /// maybe embed the html derived data
+          /// in the log json? or return array?
+          return { json: one.data, html: two }
+        })
     }
   }
-}
-/**
-Usage axmple:
+}())
+/*
+Usage example:
 const {
   validateLogJson
   validateAndMineLogHtml
--- routes/log.js
+++ routes/log.js
@@ -1,13 +1,17 @@
 const
-  express = require('express'),
-  router = express.Router(),
-  { join } = require('path'),
-  axios = require('axios'),
-  { Observable } = require('rxjs/Rx'),
-  { isError,
-    isObject,
-    isString } = require('lodash'),
-  { load } = require('cheerio')
+  express = require('express')
+  ,router = express.Router()
+  ,{ join } = require('path')
+  ,axios = require('axios')
+  // ,{ Observable } = require('rxjs/Rx')
+  ,isError = require('lodash/fp/isError')
+  ,isObject = require('lodash/fp/isObject')
+  ,isString = require('lodash/fp/isString')
+  ,{ load } = require('cheerio')
+  ,{ validateLogJson
+    ,validateAndMineLogHtml
+    ,TakeNTimesWithDelay$
+    ,Combined$ } = require('../log/helpers')
 
 /* /api/12345 */
 router.get('/:logId', (req, res, next) => {
@@ -17,89 +21,36 @@ router.get('/:logId', (req, res, next) => {
   const pathRegex = /^\/?\d+$/g
   if (!pathRegex.test(logId)) res.status(400).end()
   else {
-    ///// needs debugging
     const
-      htmlUrl = join('http://logs.tf/', logId),
-      jsonUrl = join('http://logs.tf/json/', logId)
+      htmlUrl = 'http://' + join('logs.tf/', logId),
+      jsonUrl = 'http://' + join('logs.tf/json/', logId)
 
-    const json$ = Observable
-      .fromPromise(axios(jsonUrl))
-      //.map(x => x.data)
-      .map(validateLogJson)
-      .retryWhen(tenTimesWithDelay)
+    // axios(jsonUrl).then(console.log).catch(console.warn)
+    const json$ = TakeNTimesWithDelay$({
+      promise: () => axios(jsonUrl),
+      validationFn: validateLogJson
+    })
 
-    const html$ = Observable
-      .fromPromise(axios(htmlUrl))
-      //.do(console.log, console.warn, console.info)
-      //.map(x => x.data)
-      .map(validateAndMineLogHtml)
-      .retryWhen(tenTimesWithDelay)
+    const html$ = TakeNTimesWithDelay$({
+      promise: () => axios(htmlUrl),
+      validationFn: validateAndMineLogHtml
+    })
 
-    // combined lastest from html$ and json$
-    Observable
-      .combineLatest(json$, html$, () => {
-        if (isError(json$))
-          return new Error('JSON is error:' + json$)
-        else if (isError(html$))
-          return new Error('HTML is error:' + html$)
-        /// maybe embed the html derived data
-        /// in the log json?
-        return { json: json$, html: html$ }
-      })
+    // combined latest from html$ and json$
+    const combined$ = Combined$(json$, html$)
+      .do(console.log)
       .subscribe(
         next => {
-          console.log(next)
-          res.send(JSON.stringify(next))
+          //console.log(next)
+          res.send(next).end()
         },
         err => {
           console.log(err)
-          res.send(JSON.stringify(err))
+          res.status(500).end()
         }
       )
 
-    /// does this even do anything?
-    function validateLogJson(x) {
-      if (isError(x)) return x
-      try {
-        return x
-      } catch(e) {
-        return new Error(`{Error: ${e}, Json: ${x}}`)
-      }
-    }
-
-    function validateAndMineLogHtml(x) {
-      const
-        $ = load(x),
-        h3 = $('.log-section > h3').html()
-
-      if ( h3 === 'Something went wrong' )
-        return new Error('logs.tf "Something went wrong" page.')
-
-      // filter out only the necessary data
-      const
-        map  = $('#log-map').html() || '',
-        time = $('.datefield').attr('data-timestamp')
-
-      if (time === undefined)
-        return new Error('Couldn\'t find data-timestamp.')
-
-      return { logId, map, time }
-    }
-
-    function tenTimesWithDelay(error$) {
-      const delay = 10 // ms
-      return error$
-        .scan((count, err) => {
-          ++count
-          return (count >= 10) ? err : count
-        }, 0)
-        .takeWhile(count => count < 10)
-        // .do(count =>
-        //    console.log(`Retry #${count} in ${delay}ms...`))
-        .delay(delay)
-    }
-
-    /// temporary
+    // temporary
     // res.send(JSON.stringify({
     //   json: `http://logs.tf/json/${ logId }`,
     //   log: `http://logs.tf/${ logId }`
